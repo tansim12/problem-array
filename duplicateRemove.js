9b928f47e2169b5e141559cae17f96d5d5c87791
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2023-08-05 21:35:38 +0600

    array last item undefind

---
+++ duplicateRemove.js
@@ -0,0 +1,10 @@
+function removeDuplicate(names) {
+    let UniqName = [];
+    for(let item of names){
+        console.log(item);
+        
+    }
+    
+}
+const nameArr = ["tansim", "ahmed ", "tashdid ", "tansim ", "joy ", "ahmed", "sojib", "joy ", "tashdid", ]
+console.log(removeDuplicate(nameArr));
